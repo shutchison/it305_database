f16c03ba1a7eaec5642e60437b737d564a725a4a
shutchison <scott.hutchison@usma.edu>
2017-05-25 19:49:31 -0400

    named tuples for primary and foreign keys added

--- named_tuples_definitions.py
+++ named_tuples_definitions.py
@@ -48,3 +48,29 @@ column_argument_names = ["table_catalog",
                          "is_nullable",
                          "unknown_thing_not_documented"] 
 column = namedtuple("column", column_argument_names)
+
+# https://docs.microsoft.com/en-us/sql/odbc/reference/syntax/sqlprimarykeys-function
+primary_key_argument_names = ["table_cat",
+                              "table_schema",
+                              "table_name",
+                              "column_name",
+                              "key_seq",
+                              "pk_name"]
+primary_key = namedtuple("primary_key", primary_key_argument_names)
+
+# https://docs.microsoft.com/en-us/sql/odbc/reference/syntax/sqlforeignkeys-function
+foreign_key_argument_names = ["pktable_cat",
+                              "pktable_schema",
+                              "pktable_name",
+                              "pkcolumn_name",
+                              "fktable_cat",
+                              "fktable_schema",
+                              "fktable_name",
+                              "fkcolumn_name",
+                              "key_seq",
+                              "update_rule",
+                              "delete_rule",
+                              "fk_name",
+                              "pk_name",
+                              "deferrability"]
+foreign_key = namedtuple("foreign_key", foreign_key_argument_names)
